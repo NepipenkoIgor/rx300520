4d97bf889d0575da22de784d8b8159bae0041e0f
Ihor <igornepipenko@gmail.com>
2020-05-30 17:41:06 +0300

    Feature: live search [ref:no-ref]

--- old/swipe/swipe.ts
+++ old/swipe/swipe.ts
@@ -1,5 +1,5 @@
-import { fromEvent, merge, Observable, zip } from "rxjs";
-import { map} from "rxjs/operators";
+import { fromEvent, iif, merge, Observable, of, zip } from "rxjs";
+import { map, pluck, switchMap } from "rxjs/operators";
 
 
 swipe(zip(
@@ -22,6 +22,13 @@ swipe(zip(
 function getX(source1$: Observable<TouchEvent>, source2$: Observable<MouseEvent>) {
     return merge(source1$, source2$)
         .pipe(
+            // switchMap((event: TouchEvent | MouseEvent) => {
+            //     return iif(
+            //         () => event instanceof TouchEvent,
+            //         of(event as TouchEvent).pipe(pluck('changedTouches', 0, 'clientX')),
+            //         of(event as MouseEvent).pipe(pluck('clientX')),
+            //     )
+            // })
             map((event: TouchEvent | MouseEvent) => {
                 if (event instanceof TouchEvent) {
                     return event.changedTouches[0].clientX
---
+++ src/live-search.ts
@@ -0,0 +1,71 @@
+import { fromEvent, Observable } from "rxjs";
+import {
+    bufferCount,
+    concatAll,
+    debounceTime,
+    distinctUntilChanged,
+    filter,
+    map,
+    pluck, reduce,
+    switchMap
+} from "rxjs/operators";
+
+export interface IResult {
+    name: string;
+    description: string;
+    owner: {
+        avatar_url: string;
+    }
+}
+
+export function liveSearch(
+    source$: Observable<KeyboardEvent>,
+    request: (text: string) => Observable<any>
+) {
+    return source$
+        .pipe(
+            debounceTime(300),
+            pluck<KeyboardEvent, string>('target', 'value'),
+            map((value) => value.trim()),
+            filter((value: string) => value.length > 3),
+            distinctUntilChanged(),
+            switchMap(request)
+        )
+}
+
+
+export function request(source$: Observable<any>) {
+    return source$.pipe(
+        pluck<any, IResult[]>('response', 'items'),
+        concatAll(),
+        map(createCart),
+        bufferCount(3),
+        reduce((resultStr: string, htmlStrs: string[]) => {
+            return resultStr += createRow(htmlStrs)
+        }, ''),
+        map((htmlStr) => htmlStr.trim().replace(/\s+(<)/g, '<'))
+    )
+}
+
+export function createCart({name, description, owner: {avatar_url}}: IResult) {
+    return `
+     <div class="col-md-4">
+        <div class="card">
+          <img class="card-img-top" src=${avatar_url} alt=${name} />
+          <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p class="card-text">${description}</p>
+          </div>
+        </div>
+     </div>
+  `
+}
+
+
+export function createRow(htmlStrings: string[]) {
+    return `
+      <div class="row">
+       ${htmlStrings.join(' ')}
+      </div>
+   `
+}
