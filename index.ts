47088c5a5e7c44a0fe8b38420d95a73bb1ecadab
Ihor <igornepipenko@gmail.com>
2020-05-30 12:28:43 +0300

    Feature: start with rxjs [ref:no-ref]

---
+++ index.ts
@@ -0,0 +1,13 @@
+// cb / promise / generator /
+
+// const sequence = new Promise((res) => {
+//     let count = 1;
+//     setInterval(() => {
+//         res(count++);
+//     })
+// });
+//
+// sequence.then((v)=>console.log(v));
+// sequence.then((v)=>console.log(v));
+
+
