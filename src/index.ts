27a39fae0ce817f0ed50f735aa1cd1323e101df5
Ihor <igornepipenko@gmail.com>
2020-05-30 13:53:10 +0300

    Feature: hot/cold [ref:no-ref]

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
 // import { Observable } from "rxjs";
 //
-// const sequence$ = new Observable((subscriber) => {
-//     let count = 1;
-//     const intervalId = setInterval(() => {
-//         if (count % 5 === 0) {
-//             clearInterval(intervalId);
-//             subscriber.complete();
-//             return
-//         }
-//         subscriber.next(count++);
-//     }, 1000)
-//     return () => {
-//         console.log('unsubscribe');
-//         clearInterval(intervalId);
-//     }
-// });
+import { Observable } from "rxjs";
+
+const socket: WebSocket = new WebSocket('wss://echo.websocket.org');
+const sequence$ = new Observable((subscriber) => {
+    socket.addEventListener('message', (e) => subscriber.next(e))
+
+
+    // let count = 1;
+    // const intervalId = setInterval(() => {
+    //     // if (count % 5 === 0) {
+    //     //     clearInterval(intervalId);
+    //     //     subscriber.complete();
+    //     //     return
+    //     // }
+    //     subscriber.next(count++);
+    // }, 1000)
+    // return () => {
+    //     console.log('unsubscribe');
+    //     clearInterval(intervalId);
+    // }
+});
 //
 // const subscription = sequence$.subscribe((v) => {
 //     console.log(v);
@@ -27,18 +33,23 @@
 //     subscription.unsubscribe();
 // }, 3000)
 
-import { interval } from "rxjs";
+// import { fromEvent, interval } from "rxjs";
+//
+//const sequence$ = fromEvent<MouseEvent>(document, 'click');
 
-const sequence$ = interval(1000);
-
-const sub1 = sequence$.subscribe((v) => {
-    console.log('Sub 1', v);
+const sub1 = sequence$.subscribe((v: any) => {
+    console.log('Sub 1', v.data);
+});
+socket.addEventListener('open', () => {
+    let count = 1;
+    const intervalId = setInterval(() => {
+        socket.send((count++).toString())
+    }, 1000)
 })
 
-sequence$.subscribe((v) => {
-    console.log('Sub 2', v);
-})
 
 setTimeout(() => {
-    sub1.unsubscribe();
-}, 3000)
+    sequence$.subscribe((v: any) => {
+        console.log('Sub 2', v.data);
+    })
+}, 5000)
